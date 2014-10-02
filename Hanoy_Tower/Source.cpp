51a36c572aee33cc443872cedd24bce6c85cd509
shaldon <shaldon@ya.ru>
2014-10-02 17:10:38 +0300

    testing

---
+++ Hanoy_Tower/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+#include <conio.h>
+
+#include <assert.h>
+
+using namespace std;
+
+void main(){
+	int var = 1;
+	cout << var++;
+	cout << endl;
+
+///test
+
+	cout << "help" << endl;
+
+}
